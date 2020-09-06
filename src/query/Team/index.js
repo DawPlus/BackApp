5d76689081b6a62f48e4c371031504b77dc918f2
dawplus <dawplus@naver.com>
2020-09-06 22:19:44 +0900

    SORT

--- src/query/Team/index.js
+++ src/query/Team/index.js
@@ -6,6 +6,7 @@ const LIST = `
         , DATE_FORMAT(updateDate, "%Y-%m-%d") AS updateDate
         , DATE_FORMAT(saveDate, "%Y-%m-%d")   AS saveDate
     FROM TEAM
+   ORDER BY saveDate DESC
 `;
 
 const SELECT = `
